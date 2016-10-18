3489240df65c5f956ede7b74bd62c3e77d7f5df0
anishpradhan <mail.anishpradhan@gmail.com>
2016-10-17 17:13:46 -0700

    dates

--- fact_check.py
+++ fact_check.py
@@ -8,17 +8,25 @@ import sys
 import dateutil.parser as dp
 import sparql
 import urllib2
+import json
 
 objects = []
 relation=[]
 ROOT = 'ROOT'
 SPARQL_SERVICE_URL = 'https://query.wikidata.org/sparql'
+global date_flag
+date_flag = 0
 
 # grammar = r"""
 #         NP: {<DT>?<JJ.*>*<NN.*>+}
 #             {}
 #     """
 # cp = nltk.RegexpParser(grammar)
+# select ?p ?o where {
+#   ?p a owl:DatatypeProperty ;
+#      rdfs:range xsd:date .
+#  <http://dbpedia.org/resource/Barack_Obama> ?p ?o .
+# }
 
 
 
@@ -79,14 +87,17 @@ def resource_extractor(labels):
                         my_list.append(values1)
                     types[values[0]]= my_list
             entities[i] = types
-            resources[label] = resource_list
+            resources[label] = resource_list        
     return resources
             # print resources
 
 
 def relation_extractor(resources):
-    print new_labels
-    print resources
+    # print new_labels
+    # sys.exit()
+    # print resources
+    print "====Relations===="
+
     for i in range(0,len(resources)):
         if str(new_labels[i]) in resources:
             for item1 in resources[new_labels[i]]:
@@ -94,10 +105,11 @@ def relation_extractor(resources):
                     link = urllib2.urlopen(item1)
                     item1 = link.geturl()
                     item1 = item1.replace('page','resource')
+                # print item1
                 for j in range(i+1,len(resources)):
                     if str(new_labels[j]) in resources:
                         for item2 in resources[new_labels[j]]:
-                            if 'dbpedia' in item1:
+                            if 'dbpedia' in item2:
                                 link = urllib2.urlopen(item2)
                                 item2 = link.geturl()
                                 item2 = item2.replace('page','resource')
@@ -105,15 +117,15 @@ def relation_extractor(resources):
                             q1=('SELECT ?r WHERE  { <'+str(item1) + '> ?r <' +str(item2)+'>}')
                             # print q1
                             try:
-                                if 'wikidata' in item1 and 'wikidata' in item2:
-                                    # print q1
-                                    result1 = doSparqlQuery(q1)
-                                    data = result1['results']['bindings'][0]
-                                    print([str(item1),str(data['r']['value']),str(item2)])
-                                    print '\n'
-                                    rel =  data['r']['value'].split('/')
-                                    relation.append(rel[-1])
-                                elif 'dbpedia' in item1 and 'dbpedia' in item2:
+                                # if 'wikidata' in item1 and 'wikidata' in item2:
+                                #     print q1
+                                #     result1 = doSparqlQuery(q1)
+                                #     data = result1['results']['bindings'][0]
+                                #     print([str(item1),str(data['r']['value']),str(item2)])
+                                #     print '\n'
+                                #     rel =  data['r']['value'].split('/')
+                                #     relation.append(rel[-1])
+                                if 'dbpedia' in item1 and 'dbpedia' in item2:
                                     result1 = sparql.query('http://dbpedia.org/sparql', q1)
                                     # print q1
                                     for row1 in result1:
@@ -129,6 +141,27 @@ def relation_extractor(resources):
                             except:
                                 pass 
 
+def date_extractor(date,resources):
+    # print resources
+    for key,val in resources.iteritems():
+        for v in val:
+            # print v
+            if 'dbpedia' in v:
+                link = urllib2.urlopen(v)
+                v = link.geturl()
+                v = v.replace('page','resource')
+                dq=('SELECT distinct ?r ?o WHERE  {  ?r a owl:DatatypeProperty ; rdfs:range xsd:date . <'+str(v) + '> ?r ?o .}')
+                resultd = sparql.query('http://dbpedia.org/sparql', dq)
+                # print resultd
+                for i,row1 in enumerate(resultd):
+                    # print i
+                    values1 = sparql.unpack_row(row1)
+                    
+                    if values1[1] == date.date():
+                        print v,values1
+                    # resources[key].extend(values1)
+
+
 with open('obama_sample.txt','r') as f:
     para = f.readline()
     for i,row in enumerate(para.split('.')):
@@ -136,33 +169,42 @@ with open('obama_sample.txt','r') as f:
         print row
         if row:
             doc = row
-            # doc = doc.replace('RT','')
             # try:
             tagged = ie_preprocess(doc)
-            # word_list = doc.split()
-            # print word_list
             # print tagged
-            # sys.exit()
             tree = nltk.ne_chunk(tagged[0])
+            print "====Tree===="
             print tree
             # tree = Tree.fromstring(str(tree)
             ent =  getNodes(tree)
+            # print ent
+            updated_labels = []
             for en in ent:
                 if 'University' in en[0] or 'College' in en[0]:
-                    label1 = en[0]+' alumni'
+                    label1 = str(en[0])+' alumni'
                     tup2 = (label1,en[1])
-                    ent.append(tup2)
+                    # print tup2
+                    updated_labels.append(tup2)
+            ent.extend(updated_labels)
+
             try:
                 date = date_parser(doc)
                 tup1 = (date,'DATE')
                 ent.append(tup1)
+                date_flag = 1
             except:
                 pass
-            # print ent
+            print "====Entities===="
+            print ent
             # sys.exit()
             resources = resource_extractor(ent)
+            # print resources
+            # print date_flag
             relation_extractor(resources)
-            objects.extend(ent)
+            if date_flag == 1:
+                date_extractor(date,resources)
+                date_flag = 0
+            # objects.extend(ent)
         # except:
             #     pass
                 # with open('logs.csv','ab') as csvf:
